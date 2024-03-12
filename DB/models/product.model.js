afc14d5c106a84c6c1ab572eca86a2afd709fa38
Ali Khaled <alielsaadany94@gmail.com>
2024-03-12 23:37:04 +0200

    -

--- DB/models/product.model.js
+++ DB/models/product.model.js
@@ -1,26 +1,41 @@
 import { Schema, model } from "mongoose";
-const productSchema = Schema({
-  name: { type: String, required: true },
-  prices: [
-    {
-      sizeName: { type: String, required: true },
-      sizePrice: { type: String, required: true },
+const productSchema = Schema(
+  {
+    name: { type: String, required: true },
+    prices: [
+      {
+        sizeName: { type: String, required: true },
+        sizePrice: { type: String, required: true },
+      },
+    ],
+    description: { type: String, required: true },
+    category: { type: Schema.Types.ObjectId, ref: "Category" },
+    resturant: { type: Schema.Types.ObjectId, ref: "Resturant" },
+    extra: [
+      {
+        itemName: { type: String, required: true },
+        price: { type: String, required: true },
+      },
+    ],
+    resturantCategory: {
+      type: Schema.Types.ObjectId,
+      ref: "Resturant.subCategories",
+      required: true,
     },
-  ],
-  description: { type: String, required: true },
-  category: { type: Schema.Types.ObjectId, ref: "Category" },
-  resturant: { type: Schema.Types.ObjectId, ref: "Resturant" },
-  extra: [
-    {
-      itemName: { type: String, required: true },
-      price: { type: String, required: true },
-    },
-  ],
-  resturantCategory: {
-    type: Schema.Types.ObjectId,
-    ref: "Resturant.subCategories",
-    required: true,
   },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+
+productSchema.virtual("resturantSubCategory", {
+  ref: "Resturant",
+  localField: "resturantCategory",
+  foreignField: "subCategories._id",
 });
+
 
 export const Product = model("Product", productSchema);
--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -3,25 +3,44 @@ import { Resturant } from "../../../DB/models/resturant.model.js";
 import { Category } from "../../../DB/models/category.model.js";
 
 export const createProduct = async (req, res, next) => {
-    let isResturant = await Resturant.findById(req.body.resturant);
-    if (!isResturant) return next(new Error("Resturant Not Found"));
-    let isCategory = await Category.findById(req.body.category);
-    if (!isCategory) return next(new Error("Category Not Found"));
-   await Product.create(req.body);
+  let isResturant = await Resturant.findById(req.body.resturant);
+  if (!isResturant) return next(new Error("Resturant Not Found"));
+  let isCategory = await Category.findById(req.body.category);
+  if (!isCategory) return next(new Error("Category Not Found"));
+  await Product.create(req.body);
   return res.json({ success: true, message: "Product Added Successfully" });
 };
 
 export const getProducts = async (req, res, next) => {
-  const products = await Product.find().populate(["resturant", "category"]);
+  let products = await Product.find().populate([
+    {
+      path: "resturant",
+      select: "name phone address openingTime closingTime",
+    },
+    "category",
+    {
+      path: "resturantSubCategory",
+      select: "subCategories",
+    },
+  ]);
+
+  products.map((product) => {
+    let subCategory = product.resturantSubCategory[0].subCategories.filter(
+      (subCategoryItem) =>
+        subCategoryItem._id.toString() == product.resturantCategory.toString()
+    )[0];
+
+    product.resturantSubCategory = subCategory;
+  });
+
   return res.json({ success: true, products });
-}
- 
- export const deleteProduct = async (req, res, next) => {
-   const product = await Product.findById(req.params.id);
-   if (!product) {
-     return next(new Error("Product Not Found"));
-   }
-   await product.deleteOne();
-   return res.json({ success: true, message: "Product Deleted Successfully" });
- }
- 
+};
+
+export const deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new Error("Product Not Found"));
+  }
+  await product.deleteOne();
+  return res.json({ success: true, message: "Product Deleted Successfully" });
+};
