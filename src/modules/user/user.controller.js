2e57f98489f930a5632b30a0e9d57a205b3a481f
ali1kh <alielsaadany94@gmail.com>
2024-07-15 02:28:06 +0300

    handle

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -83,6 +83,9 @@ export const login = async (req, res, next) => {
 };
 
 export const updateAcc = async (req, res, next) => {
+  if ( !req.body.name && !req.body.email && !req.body.phone && !req.body.password && !req.body.area) {
+    return next(new Error("You Must Enter At Least One Field"));
+  }
   if (req.body.email) {
     const emailExits = await User.findOne({ email: req.body.email });
     if (emailExits) {
